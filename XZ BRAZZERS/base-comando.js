43e96c2a4a6eb4f3e64a17f20462d4375305c0cc
FERNANDOJB100 <115410721+ELFERNANDOJB100@users.noreply.github.com>
2022-11-08 23:24:20 -0300

    Add files via upload

---
+++ XZ BRAZZERS/base-comando.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js")
+
+module.exports = {
+  name: "", // nombre
+  description: "", // descripcion
+  type: Discord.ApplicationCommandType.ChatInput,
+
+  run: async (client, interaction) => {
+
+
+  }
+}
---
+++ XZ BRAZZERS/comandos/utilidad/ping.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js")
+
+module.exports = {
+  name: "ping", // Coloque el name del comando
+  description: "ping del bot que mas.", // descripcion
+  type: Discord.ApplicationCommandType.ChatInput,
+
+  run: async (client, interaction) => {
+
+    let ping = client.ws.ping;
+
+    let embed_1 = new Discord.EmbedBuilder()
+    .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
+    .setDescription(`hola ${interaction.user}, estoy  \`calculando...\`.`)
+    .setColor("Random");
+
+    let embed_2 = new Discord.EmbedBuilder()
+    .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
+    .setDescription(`hola ${interaction.user}, mi ping es de \`${ping}ms\`.`)
+    .setColor("Random");
+    interaction.reply({ embeds: [embed_1] }).then( () => {
+        setTimeout( () => {
+            interaction.editReply({ embeds: [embed_2] })
+        }, 2000)
+    })
+  }
+}
